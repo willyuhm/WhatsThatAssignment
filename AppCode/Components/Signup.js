509a6e182268ad860cc445fd1500854c877b7620
willyuhm <billiuscummings@hotmail.com>
2023-04-26 00:34:30 +0100

    Signup works backend but no response to user

--- AppCode/Components/Signup.js
+++ AppCode/Components/Signup.js
@@ -7,6 +7,7 @@ export default class Signup extends Component {
     super(props);
 
     this.state = {
+      is_loading: true,
       first_name: null,
       last_name: null,
       email: null,
@@ -23,7 +24,7 @@ export default class Signup extends Component {
           style={styles.input}
           placeholder="Enter First Name"
           onChangeText={first_name => this.setState({ first_name })}
-          defaultValue={this.state.first_name}
+          value={this.state.first_name}
         />
 
         <Text style={styles.inputLabel}>Last Name:</Text>
@@ -31,7 +32,7 @@ export default class Signup extends Component {
           style={styles.input}
           placeholder="Enter Last Name"
           onChangeText={last_name => this.setState({ last_name })}
-          defaultValue={this.state.last_name}
+          value={this.state.last_name}
         />
 
         <Text style={styles.inputLabel}>Email:</Text>
@@ -39,7 +40,7 @@ export default class Signup extends Component {
           style={styles.input}
           placeholder="Enter Email"
           onChangeText={email => this.setState({ email })}
-          defaultValue={this.state.email}
+          value={this.state.email}
         />
 
         <Text style={styles.inputLabel}>Password:</Text>
@@ -47,11 +48,40 @@ export default class Signup extends Component {
           style={styles.input}
           placeholder="Enter Password"
           onChangeText={password => this.setState({ password })}
-          defaultValue={this.state.password}
+          value={this.state.password}
+        />
+
+        <Button
+          title="Sign up"
+          onPress={() => this.signup()}
         />
       </View>
       
     );
+  }
+
+  signup(){
+    let to_send = {
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    return fetch("http://localhost:3333/api/1.0.0/user", {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(to_send)
+    })
+    .then((response) => {
+      Alert.alert("Successfully signed up");
+      // this.getData();
+    })
+    .catch((error) => { 
+      console.log(error);
+    })
   }
 
 }
